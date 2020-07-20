8e6fc8442495673aedab989078e9442b8d72ed39
Wei.xiao1 <wxiao1@yitu-inc.intra>
2020-07-20 16:23:48 +0800

    Update imagenet.py

--- pytorch/imagenet.py
+++ pytorch/imagenet.py
@@ -237,7 +237,7 @@ def train(train_loader, model, criterion, optimizer, epoch, use_cuda):
         data_time.update(time.time() - end)
 
         if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda(async=True)
+            inputs, targets = inputs.cuda(), targets.cuda()
         inputs, targets = torch.autograd.Variable(inputs), torch.autograd.Variable(targets)
 
         # compute output
